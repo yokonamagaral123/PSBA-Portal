36522acfa9b18a8d453a06807a578d07814df72e
rykngrfl <grafilryan@gmail.com>
2025-05-24 03:01:43 +0800

    removal of sup functions except req & payslip above req

---
+++ frontend/src/hr/HrPayslip/HrPayslip.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const AdminPayroll = () => {
+  return (
+    <div className="content">
+      <h1>Payslip</h1>
+      <p>Payslip information per-user</p>
+      {/* Add payroll management features here */}
+    </div>
+  );
+};
+
+export default AdminPayroll;
